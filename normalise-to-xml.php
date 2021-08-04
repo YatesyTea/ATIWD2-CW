4eff93107a5deccd75061c7209b73f3afe9e8658
YatesyTea <danyates1997@gmail.com>
2021-08-05 00:39:52 +0100

    Update normalise-to-xml.php

--- normalise-to-xml.php
+++ normalise-to-xml.php
@@ -1,14 +1,70 @@
 <?php
 
+# Setting measurements.
+$time_taken = microtime(true);
+$loops = 0;
 
-
-$st = microtime(true);
-
-# Function to convert from csv to XML
+# Function to convert from CSV to XML formats.
 function convert($in, $out) {
+
+    # Create New Temporary XML Document.
+    $temp_xml  = new DomDocument('1.0',"UTF-8");
+    $temp_xml->formatOutput = true;
     
+
+    /*  Here we want to set station
+        id:
+        name: 
+        geocode: (combo of lat and long)
+    */
+    $csv_in = fopen($in, 'r');
+    $first_line = fgets($csv_in);
+    $second_line = fgets($csv_in);
+
+    # Setting ID of station.
+
+    # Setting Name (location) of station.
+
+    # Setting Geocode (Lat + Long) of station.
+
+    fclose($csv_in);
+
+
+
     # Opening our Csv file.
     $csv_in = fopen($in, 'r');
+
+    #   Use CSV Header line to generate names for usage in XML creation
+    $header_line = fgets($csv_in);
+    print_r($header_line);
+    $temp_list = list($siteID,$ts,$nox,$no2,$no,$PM10,$NVPM10,$VPM10,$NVPM2_5,$PM2_5,$VPM2_5,$CO,$O3,$SO2,$loc,$lat,$long) = explode(',', $header_line);
+    $header_names = array();
+    
+    /*  Loop through $temp_list and push the 
+        header string values to $header_names*/
+    for ($i = 1; $i <= count($temp_list)-4; $i++) {
+        array_push($header_names, $temp_list[$i]);
+    }
+    
+
+    while(!feof($csv_in)) {
+        # Load line.
+        $current_line = fgets($csv_in);
+
+        /*  Break it down into pieces to use as tags.
+            CSV headers is: siteID,ts,nox,no2,no,pm10,nvpm10,vpm10,nvpm2.5,pm2.5,vpm2.5,co,o3,so2,loc,lat,long.*/
+        [$siteID, $ts, $NOx, $NO2, $NO, $PM10, $NVPM10, $VPM10, $NVPM2_5, $PM2_5, $VPM2_5, $CO, $O3, $SO2, $loc, $long, $lat] = explode(';', $current_line);
+
+        # Taking values we want.
+        $current_line_array = array($ts, $NOx, $NO2, $NO, $PM10, $NVPM10, $VPM10, $NVPM2_5, $PM2_5, $VPM2_5, $CO, $O3, $SO2);
+
+        # Loop through $current_line_array and add values to record wherever value != null.
+
+        
+        
+    }
+
+
 
 }
 
@@ -18,7 +74,7 @@ function convert($in, $out) {
 function generateFileName($prefix, $num, $postfix){
     $str_num = (string)$num;
     $concant = $prefix . $str_num . $postfix;
-    echo($concant.'<br>');
+    #echo($concant.'<br>'); # Is just here for when genFileName doesn't work.
     return $concant;
 }
 
@@ -30,6 +86,8 @@ function generateFileName($prefix, $num, $postfix){
 $csv_prefix = 'csvData/data_';
 $xml_prefix = 'xmlData/data_';
 
+/*  Instead of doing this could take the filenames from the dir in this task.
+    Then replace each occurance of csv with xml for the xml files.*/
 $data_nums_int = array(
     188, 203, 206, 209, 213, 215, 228, 270, 271,
     375, 395, 447, 452, 459, 463, 481, 500, 501
@@ -56,4 +114,8 @@ for ($i = count($data_nums_int) - 1; $i >= 0; $i--) {
 # Using the CSV to XML function using loops through the generated file names.
 for ($i = count($data_nums_int) - 1; $i >= 0; $i--) {
     convert($csvfilenames[$i],$xmlfilenames[$i]);
+    $loops++;
+    echo($loops.' files converted.<br>');
 }
+
+echo('Complete in '.$time_taken.'ms.');
