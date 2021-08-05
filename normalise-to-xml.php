d86f42f59faaeaba76b4c367cab8d9fc274f06b8
YatesyTea <danyates1997@gmail.com>
2021-08-05 10:03:28 +0100

    it writes files but doesn't quite work

--- normalise-to-xml.php
+++ normalise-to-xml.php
@@ -8,36 +8,54 @@ $time_taken = microtime(true);
 $loops = 0;
 
 # Function to convert from CSV to XML formats.
-function convert($in, $out) {
+function convert($in, $out)
+{
 
-    # Create New Temporary XML Document.
+    # Create New XML writer that will contain data we shove in it.
     $xml = new XMLWriter();
-    $xml ->openMemory();
-    $xml ->startDocument('1.0', 'UTF-8');
-    
-    
+    $xml->openMemory();
+    $xml->startDocument('1.0', 'UTF-8');
+
+
     $csv_in = fopen($in, 'r');
     while (!feof($csv_in)) {
         $current_line = fgetcsv($csv_in);
 
-        if($current_line[0] = 'siteID'){
+        # Section to create station and it's attributes.
+        if ($current_line[0] == 'siteID') {
+            print($current_line[0] . 'proccing<br><br>');
             # Setting what the titles are, as an array to check against later.
             $header_line = $current_line;
+            # Load second line to set station stuff.
+            $current_line = fgetcsv($csv_in);
+
+            # Create Geocode
+            $geocode = ((string)$current_line[15] . ',' . (string)$current_line[16]);
 
             /*  Here we want to set station
             id:
             name: 
             geocode: (combo of lat and long)*/
-            $xml -> startElement('station');
-
-
-
+            $xml->startElement('station');
+            $xml->writeAttribute('id', $current_line[0]);
+            $xml->writeAttribute('name', $current_line[14]);
+            $xml->writeAttribute('geocode', $geocode);
         }
-        print('Count: '.count($current_line)); # Adresses length of array.
-    }
-    $xml -> endElement('station');
-    $xml -> endDocument();
 
+        # Starting a record before filling in it's parameters.
+        $xml->startElement('rec');
+
+        for ($i = 1; $i < count($header_line); $i++) {
+            $xml ->writeAttribute($header_line[$i], $current_line[$i]);
+        }
+        # Closes 'rec'
+        $xml->endElement();
+        // print('Count: '.count($current_line)); # Adresses length of array.
+    }
+    # Closing the XML writer.
+    $xml->endElement();
+    $xml->endDocument();
+    # Offloading all XML data to the XML file.
     file_put_contents($out, $xml->outputMemory());
 
     # Setting ID of station.
@@ -56,20 +74,21 @@ function convert($in, $out) {
 
     # Wasting header, with faster fgets.
 
-    
 
 
-        # Load as array
+
+    # Load as array
 
 
-        /*  Loop through $current_line_array and add values to record wherever value != null.
+    /*  Loop through $current_line_array and add values to record wherever value != null.
             Skipping out the first value, and the last 3, since they are the repeating values.*/
 }
 
 
 
 # Takes in all the inputs, concantenates them, and returns the value.
-function generateFileName($prefix, $num, $postfix){
+function generateFileName($prefix, $num, $postfix)
+{
     $str_num = (string)$num;
     $concant = $prefix . $str_num . $postfix;
     #echo($concant.'<br>'); # Is just here for when genFileName doesn't work.
@@ -88,7 +107,7 @@ $xml_prefix = 'xmlData/data_';
     Then replace each occurance of csv with xml for the xml files.*/
 $data_nums_int = array(
     188, 203, 206, 209, 213, 215, 228, 270, 271,
-    375, 395, 447, 452, 459, 463, 481, 500, 501
+    375, 395, 447, 452, 459, 463, 500, 501
 );
 
 $csv_postfix = '.csv';
@@ -110,10 +129,10 @@ for ($i = count($data_nums_int) - 1; $i >= 0; $i--) {
 }
 
 # Using the CSV to XML function using loops through the generated file names.
-// for ($i = count($data_nums_int) - 1; $i >= 0; $i--) {
-//     convert($csvfilenames[$i],$xmlfilenames[$i]);
-//     $loops++;
-//     echo($loops.' files converted.<br>');
-// }
-convert('csvData/data_188.csv', 'xmlData/data_188.xml');
-echo('Complete in '.($time_taken/1000000).'ms.');
+for ($i = count($data_nums_int) - 1; $i >= 0; $i--) {
+    convert($csvfilenames[$i],$xmlfilenames[$i]);
+    $loops++;
+    echo($loops.' files converted.<br>');
+}
+// convert('csvData/data_188.csv', 'xmlData/data_188.xml');
+echo ('Complete in ' . ($time_taken / 1000000) . 'ms.');
--- temp.php
+++ temp.php
@@ -10,3 +10,7 @@ for ($i = 1; $i < (count($current_line) - 3); $i++) {
     }
 
 }
+
+/*      print_r($current_line); # Addresses whole array.       
+        print($current_line[1]); # Adresses individual element in array. 
+        print('<br><br><br>'); */
